da9d07f3d870ca71ea2bf10628d06a0198b48fd4
Gabriel <gabrielcarra.p@hotmail.com>
2024-10-25 18:04:27 -0300

    ajuste html

--- src/app/services/chamado/chamado.service.ts
+++ src/app/services/chamado/chamado.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../../../environment/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Chamado } from '../../model/interfaces/Chamado';
@@ -9,7 +8,7 @@ import { CriarChamado } from '../../model/interfaces/CriarChamado';
   providedIn: 'root'
 })
 export class ChamadoService {
-  private Api_URL = environment.ApiUrl;
+  // private Api_URL = environment.ApiUrl;
   private httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json'
@@ -20,20 +19,20 @@ constructor(
   private http: HttpClient
 ) { }
 
-  getAllChamados():Observable<Array<Chamado>>{
-    return this.http.get<Array<Chamado>>(`${this.Api_URL}/chamados`,this.httpOptions)
+  // getAllChamados():Observable<Array<Chamado>>{
+  //   return this.http.get<Array<Chamado>>(`${this.Api_URL}/chamados`,this.httpOptions)
+  // }
+
+  // getChamadoByPk(codigo: bigint):Observable<Chamado>{
+  //   return this.http.get<Chamado>(`${this.Api_URL}/${codigo}`,this.httpOptions)
+  // }
+
+  // createChamado(chamado: CriarChamado):Observable<Array<Chamado>>{
+  //   return this.http.post<Array<Chamado>>(`${this.Api_URL}/chamados`,chamado,this.httpOptions)
+  // }
+
+  // cancelarChamado(codigo:bigint):Observable<Array<Chamado>>{
+  //   return this.http.post<Array<Chamado>>(`${this.Api_URL}/cancelar/${codigo}`,this.httpOptions)
   }
 
-  getChamadoByPk(codigo: bigint):Observable<Chamado>{
-    return this.http.get<Chamado>(`${this.Api_URL}/${codigo}`,this.httpOptions)
-  }
-
-  createChamado(chamado: CriarChamado):Observable<Array<Chamado>>{
-    return this.http.post<Array<Chamado>>(`${this.Api_URL}/chamados`,chamado,this.httpOptions)
-  }
-
-  cancelarChamado(codigo:bigint):Observable<Array<Chamado>>{
-    return this.http.post<Array<Chamado>>(`${this.Api_URL}/cancelar/${codigo}`,this.httpOptions)
-  }
-
-}
+// }
